b8a2df59ba21078ca2e3b6aaa0b5094f859595d2
Mkratenstein <mattkratenstein@gmail.com>
2025-06-15 10:08:00 -0400

    fix: handle interaction timeouts by responding immediately

--- src/commands/wted.js
+++ src/commands/wted.js
@@ -73,10 +73,11 @@ module.exports = {
                     return interaction.reply({ content: 'Could not find the specified voice channel.', flags: [4096] });
                 }
 
+                // Reply immediately
                 await interaction.reply({ content: 'Starting the wTed radio bot...', flags: [4096] });
-                log('Attempting to join voice channel');
 
                 try {
+                    log('Attempting to join voice channel');
                     const connection = joinVoiceChannel({
                         channelId: voiceChannel.id,
                         guildId: guild.id,
@@ -129,9 +130,12 @@ module.exports = {
                     voiceManager.set(guild.id, { connection, player, timer });
                     log('Voice manager entry created', { guildId: guild.id });
 
+                    // Send a follow-up message
+                    await interaction.followUp({ content: 'Successfully connected to voice channel!', flags: [4096] });
+
                 } catch (error) {
                     log('Error during voice setup', { error: error.message });
-                    throw error;
+                    await interaction.followUp({ content: 'Failed to connect to voice channel. Please try again.', flags: [4096] });
                 }
 
             } else if (subcommand === 'end') {
@@ -151,13 +155,16 @@ module.exports = {
                     return interaction.reply({ content: 'The bot is not currently playing.', flags: [4096] });
                 }
 
+                // Reply immediately
+                await interaction.reply({ content: 'Stopping the wTed radio bot...', flags: [4096] });
+
                 log('Stopping bot and cleaning up resources');
                 const { connection, timer } = voiceManager.get(guild.id);
                 clearTimeout(timer);
                 connection.destroy();
                 voiceManager.delete(guild.id);
 
-                return interaction.reply({ content: 'The wTed bot has been stopped.', flags: [4096] });
+                await interaction.followUp({ content: 'The wTed bot has been stopped.', flags: [4096] });
 
             } else if (subcommand === 'restart') {
                 log('Restart command executed', {
@@ -176,6 +183,9 @@ module.exports = {
                     return interaction.reply({ content: 'The bot is not currently playing.', flags: [4096] });
                 }
 
+                // Reply immediately
+                await interaction.reply({ content: 'Restarting the timer...', flags: [4096] });
+
                 log('Restarting timer');
                 const { timer } = voiceManager.get(guild.id);
                 clearTimeout(timer);
@@ -189,7 +199,7 @@ module.exports = {
 
                 voiceManager.get(guild.id).timer = newTimer;
 
-                await interaction.reply({ content: 'The timer has been restarted for 3 hours.', flags: [4096] });
+                await interaction.followUp({ content: 'The timer has been restarted for 3 hours.', flags: [4096] });
                 const textChannel = guild.channels.cache.get(TEXT_CHANNEL_ID);
                 if (textChannel) {
                     await textChannel.send('The wTed bot timer has been restarted for another 3 hours.');
@@ -197,7 +207,15 @@ module.exports = {
             }
         } catch (error) {
             log('Error in wted command', { error: error.message, stack: error.stack });
-            await interaction.reply({ content: 'There was an error executing this command.', flags: [4096] });
+            try {
+                if (!interaction.replied) {
+                    await interaction.reply({ content: 'There was an error executing this command.', flags: [4096] });
+                } else {
+                    await interaction.followUp({ content: 'There was an error executing this command.', flags: [4096] });
+                }
+            } catch (replyError) {
+                log('Error sending error message', { error: replyError.message });
+            }
         }
     },
 }; 
